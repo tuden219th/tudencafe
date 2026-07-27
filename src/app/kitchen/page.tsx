3fd9779c6178af4f05a96880c531cdf9ad572707
tudencafe <tuden219th@gmail.com>
2026-07-27 11:11:48 +0700

    Fix kitchen refresh data

--- src/app/kitchen/page.tsx
+++ src/app/kitchen/page.tsx
@@ -1,6 +1,8 @@
 import { supabase } from "@/lib/supabase";
 import KitchenClient from "@/components/kitchen/KitchenClient";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default async function KitchenPage() {
 
